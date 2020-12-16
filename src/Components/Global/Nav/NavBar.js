96236e45dcc293fdb88b5bc07f3538dda818ae13
Jordania Pereyra <Jordania.Pereyra@gmail.com>
2020-12-16 15:24:39 -0500

    header update

--- src/Components/Global/Nav/NavBar.js
+++ src/Components/Global/Nav/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Logo2 from '../../../Assets/NavbarImages/BYTA_Logo.png'
-// import "./NavBar.css"
+import "./NavBar.css"
 // import NavBarSearch from './NavBarSearch/NavBarSearch'
 
 
@@ -12,48 +12,44 @@ function NavBar() {
         <a href="/"><img src={Logo2} className="brand-title" alt="byta" /></a>
       </div>
 
-
-
       <a href="/" className="toggle-button">
         <span className="bar"></span>
         <span className="bar"></span>
         <span className="bar"></span>
       </a>
 
-
-
       <div className="navbar_menu_options">
         <div className="dropdown" id="navbar-links">
-          <ul>
-            <li className="navbarLi"><a href="/about" >About Us</a></li>
-            <div className="dropdown-content">
-            </div>
-          </ul>
+          {/* <ul> */}
+          <div className="navbarLi"><a href="/about" >About Us</a></div>
+          <div className="dropdown-content">
+          </div>
+          {/* </ul> */}
         </div>
 
         <div className="dropdown" id="navbar-links">
-          <ul>
-            <li className="navbarLi"><a href="/Programs" >Our Programs</a></li>
-          </ul>
+
+          <div className="navbarLi"><a href="/Programs" >Our Programs</a></div>
+
         </div>
 
         <div className="dropdown" id="navbar-links">
-          <ul >
-            <li className="navbarLi"><a href="/getinvolved" >Get Involved</a></li>
-            <div className="dropdown-content">
-              <a href="/getinvolved/becomesponsor">Become a Sponsor</a>
-              <a href="/UnderConstruction">Volunteer</a>
-              <a href="/UnderConstruction">Donate</a>
-            </div>
-          </ul>
+
+          <div className="navbarLi"><a href="/getinvolved" >Get Involved</a></div>
+          <div className="dropdown-content">
+            <a href="/getinvolved/becomesponsor">Become a Sponsor</a>
+            <a href="/UnderConstruction">Volunteer</a>
+            <a href="/UnderConstruction">Donate</a>
+          </div>
+
         </div>
 
         <div className="dropdown" id="navbar-links">
-          <ul >
-            <li className="navbarLi">
-              <a href="/ContactUs" >Contact Us</a>
-            </li>
-          </ul>
+
+          <div className="navbarLi">
+            <a href="/ContactUs" >Contact Us</a>
+          </div>
+
         </div>
       </div>
 
@@ -61,9 +57,9 @@ function NavBar() {
 
 
 
-      <ul className="navbar-button">
-        <li><a href="/supportUs/donate" className="navbarButtonText" id="donateColor">Support Us</a></li>
-      </ul>
+      {/* <ul className="navbar-button"> */}
+      <a href="/supportUs/donate" className="navbar-button" id="donateColor">Support Us</a>
+      {/* </ul> */}
 
 
       {/* <ul id="navbar-links"> */}
