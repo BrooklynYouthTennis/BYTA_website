401d258c46457bc294b24ccdee2321c5986a84f6
Jordania Pereyra <Jordania.Pereyra@gmail.com>
2020-12-16 17:14:52 -0500

    header update

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,16 +5,11 @@ import "./Header.css";
 function Header(props) {
   return (
     <div className="hero-container">
-      <img className="hero-image" src={HeroImg} alt="BYTA"></img>
-
       <div className="hero-text-block">
         <h2 className="heroTitle">The motto</h2>
-        <div className="heroSlogan_block">
-          <div className="heroSlogan">
-            Talk with your racquet, play with your heart.
+        <div className="heroSlogan">
+          Talk with your racquet, play with your heart.
         </div>
-        </div>
-
       </div>
     </div>
   );
