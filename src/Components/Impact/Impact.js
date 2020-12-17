b2cf823393c653af6c6806b5e7d0a425ade58c83
Jordania Pereyra <Jordania.Pereyra@gmail.com>
2020-12-17 15:33:56 -0500

    styled impact sesction

--- src/Components/Impact/Impact.js
+++ src/Components/Impact/Impact.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import './Impact.css'
+// import '../../Assets/'
 
 
 export default function Impact() {
@@ -10,37 +12,46 @@ export default function Impact() {
       <div className="Impact_grid">
 
         <div className="Impact_grid_college">
-          Impact
+          <h1>Impact</h1>
 
-          Our graduates enroll at top colleges and universities, such as:
+          <p>
+            Our graduates enroll at top colleges and universities, such as:
 
-          Colby
-          Columbia University
-          Dartmouth College
-          Hampton University
-          Harvard University
-          Howard University
-          Morehouse College
-          Mt. Holyoke
-          NYU
-          Rutgers University
-          Syracuse University
-          Temple University
-          Tulane University
-          Xavier University of LA
+            <ul>
+              <li>Colby</li>
+              <li>Columbia University</li>
+              <li>Dartmouth College</li>
+              <li> Hampton University</li>
+              <li>Harvard University</li>
+              <li>Howard University</li>
+              <li>Morehouse College</li>
+              <li>Mt. Holyoke</li>
+              <li>NYU</li>
+              <li>Rutgers University</li>
+              <li> Syracuse University</li>
+              <li>Temple University</li>
+              <li> Tulane University</li>
+              <li>Xavier University of LA</li>
+            </ul>
+          </p>
+
         </div>
 
-        <div className="Impact_grid_athletes"></div>
-        The Brooklyn Youth Tennis Association (BYTA), a not-for-profit organization, opens a new door for many inner-city boys and girls, ages 7-18, to learn the game of tennis. We enroll about 1,000 students during the school year and 150-300 in our summer program.
+        <div className="Impact_grid_athletes">
+          <h1>Our  Athletes</h1>
+          <p>
+            The Brooklyn Youth Tennis Association (BYTA), a not-for-profit organization, opens a new door for many inner-city boys and girls, ages 7-18, to learn the game of tennis. We enroll about 1,000 students during the school year and 150-300 in our summer program.
 
-At BYTA, we insist on academic achievement and offer educational support. The average GPA of our participants is 3.1 (2.0 or higher GPA is required to remain in the program).
+            At BYTA, we insist on academic achievement and offer educational support. The average GPA of our participants is 3.1 (2.0 or higher GPA is required to remain in the program).
 
- 90% of our participants earn a high school diploma, compared to the 67% of students who graduate from high school citywide and only 51% in Brooklyn.
+            90% of our participants earn a high school diploma, compared to the 67% of students who graduate from high school citywide and only 51% in Brooklyn.
 
-25% earn tennis scholarships to leading colleges
-65% receive non-athletic financial aid
-60% attend college at leading private and public institutions
+            25% earn tennis scholarships to leading colleges
+            65% receive non-athletic financial aid
+            60% attend college at leading private and public institutions
+          </p>
 
+        </div>
       </div>
     </div>
   )
