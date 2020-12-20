920d72c6ec684922d382fba76c955b3e22f42d53
Jordania Pereyra <Jordania.Pereyra@gmail.com>
2020-12-19 23:57:35 -0500

    donate form 1

--- src/Pages/DonatePage/DonatePage.js
+++ src/Pages/DonatePage/DonatePage.js
@@ -1,5 +1,8 @@
 // import React from 'react';
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+// import { useForm } from 'react-hook-form';
 // import './DonatePage.css';
 
 
@@ -8,6 +11,8 @@ function DonatePage() {
     firstName: '',
     lastName: '',
     email: '',
+
+
   });
 
   const handleFirstNameInputChange = (event) => {
@@ -34,13 +39,17 @@ function DonatePage() {
     }));
   };
 
+  let history = useHistory();
+
   const handleSubmit = (event) => {
     event.preventDefault();
     if (values.firstName && values.lastName && values.email) {
       setValid(true);
     }
-    setSubmitted(true);
+    // setSubmitted(true);
+    setSubmitted(true) && history.pushState('/');
   }
+
 
   const [submitted, setSubmitted] = useState(false);
 
@@ -52,26 +61,81 @@ function DonatePage() {
 
       <form className='register-form' onSubmit={handleSubmit}>
 
-        {valid && <div className='success-message'>Success! Thank you for registering</div>}
+        {/* {valid && <div className='success-message'>Success! Thank you for registering</div>} */}
+        {/* {valid && <div className='success-message'><Link>'/'</Link>} */}
 
-        <input
+
+
+
+        <div className="donation-container">
+          {/* <div class="donation-box"> */}
+
+          <div className="title">Choose a donation amount</div>
+
+          <div className="fields">
+            <input type="text" id="firstName" placeholder="First Name" />
+            <input type="text" id="lastName" placeholder="Last Name" />
+            <input type="text" id="email" placeholder="Email" />
+          </div>
+
+          <div className="amount">
+            <div className="button">$10</div>
+            <div className="button">$25</div>
+            <div className="button">$50</div>
+            <div className="button">$
+                <input type="text" className="set-amount" placeholder="" />
+            </div>
+          </div>
+
+          <div className="title">Choose a donation amount</div>
+
+          <div className="switch">
+            <input type="radio" class="switch-input" name="view" value="One-Time" id="one-time" checked="" />
+            <label for="one-time" className="switch-label switch-label-off">One-Time</label>
+            <input type="radio" className="switch-input" name="view" value="Monthly" id="monthly" />
+            <label for="monthly" className="switch-label switch-label-on">Monthly</label>
+            <span className="switch-selection"></span>
+          </div>
+
+          <div className="checkboxes">
+            <input type="checkbox" id="receipt" className="checkbox" />
+            <label for="receipt">Email Me A Receipt</label>
+            <br />
+            <input type="checkbox" id="anon" className="checkbox" />
+            <label for="anon">Give Anonymously</label>
+            <br />
+            <input type="checkbox" id="list" className="checkbox" />
+            <label for="list">Add Me To Email List</label>
+          </div>
+
+          <div className="confirm"></div>
+
+          <div class="donate-button"><i class="fa fa-credit-card"></i> Donate Now</div>
+        </div>
+
+        {/* </div>
+
+        </div> */}
+
+
+        {/* </div> */}
+
+
+        {/* <input
           id="first-name"
           className="form-field"
           type="text"
-          // disabled={valid}
           placeholder="First Name"
           name="firstName"
           value={values.firstName}
           onChange={handleFirstNameInputChange}
         />
-        {/* <span id="first-name-error">Please enter a first name</span> */}
         {submitted && !values.firstName && <span id='first-name-error'>Please enter a first name</span>}
 
         <input
           id="last-name"
           className="form-field"
           type="text"
-          // disabled={valid}
           placeholder="Last Name"
           name="lastName"
           value={values.lastName}
@@ -83,90 +147,17 @@ function DonatePage() {
           id="email"
           className="form-field"
           type="text"
-          // disabled={valid}
           placeholder="Email"
           name="email"
           value={values.email}
           onChange={handleEmailInputChange}
         />
-        {submitted && !values.email && <span id='email-error'>Please enter an email address</span>}
+        {submitted && !values.email && <span id='email-error'>Please enter an email address</span>} */}
 
         <input type="submit" value="Submit" />
-        {/* <button type="submit">Submit</button> */}
-
-        {/* <button>submit</button>
-        <div class='success-message'>Success! Thank you for registering</div> */}
       </form>
     </>
   );
 }
 
-// function Example() {
-//   // Declare a new state variable, which we'll call "count"
-//   const [count, setCount] = useState(0);
-
-//   return (
-//     <div>
-//       <p>You clicked {count} times</p>
-//       <button onClick={() => setCount(count + 1)}>
-//         Click me
-//       </button>
-//     </div>
-//   );
-// }
-
-
-// function DonatePage() {
-
-//   //   const [values, setValues] = useState({
-//   //     firstName: '',
-//   //     lastName: '',
-//   //     email: '',
-//   //   });
-
-//   const [count, setCount] = useState(0);
-
-//   return (
-//     <div>
-//       <p>You clicked {count} times</p>
-//       <button onClick={() => setCount(count + 1)}>
-//         Click me
-//       </button>
-//     </div>
-//   );
-// }
-
-
-//   return (
-
-//     <>
-//       <div className="donate">
-
-//         <div className="donateHeader">
-//           Choose Donation Amount
-//         </div>
-
-//         <div className="donateContainer">
-//           <div>
-//             <button className="CallAbutton">$50</button>
-//           </div>
-
-//           <button className="CallAbutton">$100</button>
-//           <button className="CallAbutton">$150</button>
-//           <button className="CallAbutton">OTHER</button>
-
-//           <div className="CallToActionNotes">
-//             <p>100% of your donations are invested in programs support and delivrery</p>
-//             <p>Donation of all amounts are appreciated and reconized on our social media.</p>
-//           </div>
-//         </div>
-
-//         <div className="DonatePageContainer">
-//           <button className=""></button>
-//         </div>
-//       </div>
-//     </>
-//   )
-
-// }
 export default DonatePage;
